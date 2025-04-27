f92b58e4613822b161d46d71192354b068eab046
18372738 <133884450+18372738@users.noreply.github.com>
2025-04-28 02:38:25 +0300

    Update registration.html

--- food_models/views.py
+++ food_models/views.py
@@ -49,12 +49,21 @@ def order_view(request):
     return render(request, 'order.html', {'form': form, 'price': total_price})
 
 
-
 def registration(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         mail = request.POST.get('mail')
         password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        if  password != password_confirm:
+            return render(request, 'registration.html', {
+                'error': 'Пароли не совпадают'
+            })
+
+        if Client.objects.filter(mail=mail).exists():
+            return render(request, 'registration.html', {
+                'error': 'Пользователь с таким email уже существует. Используйте кнопку войти.'
+            })
 
         Client.objects.create(
             name=name,
